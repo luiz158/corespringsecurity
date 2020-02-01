64b691bb1ca82c9716e78212e0e1163c9e053f94
leaven <leaven73@gmail.com>
2020-02-01 16:11:33 +0900

    lecture

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,6 +1,5 @@
 package io.security.corespringsecurity.security.configs;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
